3a79d00e3fd45dea4eac358c5ac3cbd4d615eabe
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 19:01:17 +0100

    testing accents module -- passed

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,6 +1,9 @@
 from Preln.preprocessing import Preprocessing
 
 def test_preprocessing():
+    pipeline = Preprocessing(lowercasing=False, punctuation=False, stopwords=False)
+    assert pipeline.pipeline('Ì b LóVe a hôlä NLp') == 'I b LoVe a hola NLp'
+    
     pipeline = Preprocessing(punctuation=False, stopwords=False)
     assert pipeline.pipeline('I LoVe NLp') == 'i love nlp'
     
