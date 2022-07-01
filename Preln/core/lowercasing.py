89658046bd6cd1590de2d354d60135e64fc9d7f5
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 01:37:43 +0100

    lowercase text module created

---
+++ Preln/core/lowercasing.py
@@ -0,0 +1,22 @@
+import logging
+
+def lowercasing(text, debug):
+    """
+    Method for lowercase text.
+    
+    :param text: The text to be preprocessed
+    :type: str
+    
+    :param debug: Apply debugging
+    :type: bool
+    """
+    
+    if debug: logging.basicConfig(level=logging.DEBUG)
+    
+    text = "".join(word.lower() for word in text)
+    logging.debug('-- The text is in lowercase!')
+    return text
+
+
+if __name__ == '__main__':
+  lowercasing()
