cc0d1f849210c094f661e7a2eafb86877a8efbfc
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 21:38:56 +0100

    Testing date module

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,6 +1,9 @@
 from Preln.preprocessing import Preprocessing
 
 def test_preprocessing():
+    pipeline = Preprocessing(date=True, date_format='complete', lowercasing=False, punctuation=False, stopwords=False)
+    assert pipeline.pipeline('11-02-2022') == 'Viernes 11 de Febrero de 2022'
+    
     pipeline = Preprocessing(lowercasing=False, punctuation=False, stopwords=False)
     assert pipeline.pipeline('Ì b LóVe a hôlä NLp') == 'I b LoVe a hola NLp'
     
