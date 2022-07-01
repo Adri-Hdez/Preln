fd3623daadd86ee258c5184556a13687734a6b6d
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 14:27:57 +0100

    Stopwords module testing passed

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,9 +1,12 @@
 from Preln.preprocessing import Preprocessing
 
 def test_preprocessing():
-    pipeline = Preprocessing(punctuation=False)
+    pipeline = Preprocessing(punctuation=False, stopwords=False)
     assert pipeline.pipeline('I LoVe NLp') == 'i love nlp'
     
-    pipeline = Preprocessing()
+    pipeline = Preprocessing(stopwords=False)
     assert pipeline.pipeline('I LoVe NLp{}[]?¿¡!;,:._-^*·#') == 'i love nlp'
+    
+    pipeline = Preprocessing()
+    assert pipeline.pipeline('I b LoVe a hola NLp{}[]?¿¡!;,:._-^*·#') == 'love nlp'
     
