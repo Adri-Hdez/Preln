ca110d5d639a6993da335f5eaaf4dc3b02ec7adf
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 14:18:58 +0100

    Stopwords module implemented

---
+++ Preln/core/stopwords.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import logging
+
+_EXTERNAL_PATH_STP = '../_external/stopwords.csv'
+stopwords = pd.read_csv(_EXTERNAL_PATH_STP)['stopwords'].values.tolist()
+
+def stopwords_es(text, debug):
+    """
+    Method for eliminate text punctuation.
+    
+    :param text: The text to be preprocessed
+    :type: str
+    
+    :param debug: Apply debugging
+    :type: bool
+    """
+    
+    if debug: logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+    
+    text = text.split()
+    print(text)
+    
+    text = [word for word in text if word not in stopwords]
+    print(text)
+    logging.debug('-- Stopwords cleanned!')
+    
+    return text
+
+
+if __name__ == '__main__':
+  stopwords_es()
