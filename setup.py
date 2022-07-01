97b8535a99be1448e8356aaf353f23945201b6d0
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-01 15:20:20 +0100

    Package data path added - csv files

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ setup(
     long_description_content_type='text/markdown',
     name='preln',
     packages=find_packages(include=['Preln', 'Preln.core']),
-    version='0.2.20',
+    version='0.2.21',
     description='A preprocessing libray for text in spanish',
     author='Adrián H.S & Raúl M.R',
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    test_suite='tests'
+    test_suite='tests',
+    include_package_data=True
 )
