9e63a8826a0624be1d2a47ef74f921611e376df4
Adri <adriideveloper@gmail.com>
2022-07-03 01:32:10 +0100

    Tokenizer method implemented

---
+++ Preln/core/tokenizer.py
@@ -0,0 +1,24 @@
+import logging
+from nltk.tokenize import word_tokenize
+
+def tokenizer(text, debug):
+    """
+    Method for tokenize text.
+    
+    :param text: The text to be preprocessed
+    :type: str
+    
+    :param debug: Apply debugging
+    :type: bool
+    """
+    
+    if debug: logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)8s | %(message)s')
+    
+    text = word_tokenize(text)
+    logging.debug('-- text tokenized!')
+
+    return text
+
+
+if __name__ == '__main__':
+    tokenizer()
