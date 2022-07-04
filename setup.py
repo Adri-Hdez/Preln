9e3b5c05ee9d1e1a9312f13ae8020daf89a6e8cd
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-04 15:40:53 +0100

    New long desc, requirements file + new version

--- setup.py
+++ setup.py
@@ -1,16 +1,30 @@
 from setuptools import find_packages, setup
+import pathlib
+import pkg_resources
 
-setup(
-    long_description_content_type='text/markdown',
-    name='preln',
-    packages=find_packages(include=['Preln', 'Preln.core']),
-    version='0.3.0',
-    description='A preprocessing libray for text in spanish',
-    author='Adrián H.S & Raúl M.R',
-    license='MIT',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    test_suite='tests',
-    include_package_data=True
+with pathlib.Path('requirements.txt').open() as txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(txt)
+    ]
+
+with open("README.md", "r") as fh:
+    LONG_DESC = fh.read()
+
+    setup(
+        long_description=LONG_DESC,
+        long_description_content_type='text/markdown',
+        name='preln',
+        packages=find_packages(include=['Preln', 'Preln.core']),
+        version='0.3.6',
+        description='A preprocessing libray for text in spanish',
+        author='Adrián H.S & Raúl M.R',
+        author_email='adrihs.dev@gmail.com',
+        license='MIT',
+        install_requires=install_requires,
+        setup_requires=['pytest-runner'],
+        url='https://github.com/Adri-Hdez/Preln',
+        tests_require=['pytest'],
+        test_suite='tests',
+        include_package_data=True
 )
