7d7028ec379048c594bd5a33debdd9da5907f674
Adrian Hernandez <adriideveloper@gmail.com>
2022-07-05 13:39:31 +0100

    numbers deleter method implemented

---
+++ Preln/core/numbers.py
@@ -0,0 +1,23 @@
+import logging
+import re
+
+def number_replace(text, debug):
+    """
+    Method for delete numbers.
+    
+    :param text: The text to be preprocessed
+    :type: str
+    
+    :param debug: Apply debugging
+    :type: bool
+    """
+    
+    if debug: logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)8s | %(message)s')
+    
+    text = re.sub('\d', '', text)
+    logging.debug('-- Numbers deleted!')
+    return text
+
+
+if __name__ == '__main__':
+  number_replace()
