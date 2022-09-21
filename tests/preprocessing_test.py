56c04c968a2e7d1730c821cd1d35bb2ed7346e81
Adrián <adrihs.dev@gmail.com>
2022-09-21 21:41:19 +0100

    Preprocessing class unittest

---
+++ tests/preprocessing_test.py
@@ -0,0 +1,25 @@
+from Preln.preprocessing import Preprocessing
+import unittest
+
+# ----------------------------------- Preprocessing testing units -----------------------------------
+class TestPreprocessing(unittest.TestCase):
+  def test_pipeline(self):
+    text = 'Hola, Adrián!'
+    preprocessor = Preprocessing(accents=True)
+    test = preprocessor.pipeline(text=text)
+    
+    self.assertEqual(test, ['adrian'])
+  
+  def test_filePipeline(self):
+    preprocessor = Preprocessing(accents=True)
+    test = preprocessor.filePipeline('./__testsDatafiles/sample.csv', 'Welcome')
+    
+    self.assertEqual(test, [['adrian']])
+  
+  def test_write(self):
+    # This test cannot be done because write method is not returnable
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
