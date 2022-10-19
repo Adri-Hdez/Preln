19886a820d7f8f1b9042a10ff4feeb63a139c8ab
Adrián <adrihs.dev@gmail.com>
2022-10-19 10:28:18 +0100

    Text correction added + corpus

---
+++ Preln/core/autocorrect.py
@@ -0,0 +1,31 @@
+from .__features.lev_distance import correction
+import logging
+
+def autocorrect(text, debug):
+  """
+  Method for auto-correct words in text.
+  
+  :param text: The text to be preprocessed.
+  :type: str
+  
+  :param debug: Apply debugging
+  :type: bool
+  """
+  
+  if debug: logging.basicConfig(level=logging.DEBUG, format='%(asctime)s | %(levelname)8s | %(message)s')
+  
+  text = text.split()
+  texts = []
+  
+  for word in text:
+    word_corrected = correction(word)
+    texts.append(word_corrected)
+  
+  texts = " ".join(texts)
+  logging.debug('-- Text corrected!')
+
+  return texts
+
+
+if __name__ == '__main__':
+  autocorrect()
