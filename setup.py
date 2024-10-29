9b251f0e7d1c6f31c12df52f237341e4f405fff5
Raúl Martín <raul.martin4bc@gmail.com>
2024-10-29 14:20:05 +0000

    setting up for 0.5.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 # Read the requirements from the requirements.txt file
-with open("requirements.txt", "r",encoding="utf-8") as f:
+with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
 
 # Read the long description from README.md
@@ -13,16 +13,13 @@ setup(
     long_description_content_type="text/markdown",
     name="preln",
     packages=find_packages(include=["Preln", "Preln.core", "Preln.core.__features"]),
-    version="0.5.6",
+    version="0.5.8",
     description="A preprocessing library for text in Spanish",
     author="Adrián H.S & Raúl M.R",
     author_email="adrihs.dev@gmail.com",
     license="MIT",
-    install_requires=requirements,  # Use requirements.txt here
-    setup_requires=["pytest-runner"],
+    install_requires=[],  # Use requirements.txt here
     url="https://github.com/Adri-Hdez/Preln",
-    tests_require=["pytest"],
-    test_suite="tests",
     include_package_data=True,
 )
 
