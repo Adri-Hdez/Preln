484da4d55d8d60ceaca30394c9ede7d226511f78
Raúl Martín <raul.martin4bc@gmail.com>
2024-10-29 13:22:26 +0000

    setting up for 0.5.6 version

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import find_packages, setup
 
+# Read the requirements from the requirements.txt file
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+# Read the long description from README.md
 with open("README.md", "r") as fh:
     LONG_DESC = fh.read()
 
-    setup(
-        long_description=LONG_DESC,
-        long_description_content_type="text/markdown",
-        name="preln",
-        packages=find_packages(include=["Preln", "Preln.core", "Preln.core.__features"]),
-        version="0.5.5",
-        description="A preprocessing libray for text in spanish",
-        author="Adrián H.S & Raúl M.R",
-        author_email="adrihs.dev@gmail.com",
-        license="MIT",
-        install_requires=[
-            "pandas",
-            "nltk",
-            "numpy",
-            "openpyxl",
-            "lxml",
-            "sqlalchemy",
-            "es_core_news_sm",
-        ],
-        setup_requires=["pytest-runner"],
-        url="https://github.com/Adri-Hdez/Preln",
-        tests_require=["pytest"],
-        test_suite="tests",
-        include_package_data=True,
-    )
+setup(
+    long_description=LONG_DESC,
+    long_description_content_type="text/markdown",
+    name="preln",
+    packages=find_packages(include=["Preln", "Preln.core", "Preln.core.__features"]),
+    version="0.5.6",
+    description="A preprocessing library for text in Spanish",
+    author="Adrián H.S & Raúl M.R",
+    author_email="adrihs.dev@gmail.com",
+    license="MIT",
+    install_requires=requirements,  # Use requirements.txt here
+    setup_requires=["pytest-runner"],
+    url="https://github.com/Adri-Hdez/Preln",
+    tests_require=["pytest"],
+    test_suite="tests",
+    include_package_data=True,
+)
+
